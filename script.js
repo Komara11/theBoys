a3779b258e62c0d61cb40783d3e4f329eff6c5b1
Rahesakomara <109297884+Komara11@users.noreply.github.com>
2025-05-04 01:56:02 +0700

    script.js

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ question.innerHTML = botSay()[0]
 function botStart(){
     init++
     if (init === 1){
-        setTimeout(question.innerHTML = botSay()[1], 1200)
+        setTimeout(question.innerHTML = botSay()[1], 1500)
         answer.value = ""
     }else if(init === 2){
-        setTimeout(question.innerHTML = botSay()[2], 1200)
+        setTimeout(question.innerHTML = botSay()[2], 1500)
         answer.value = ""
     }else if(init === 3){
-        setTimeout(question.innerHTML = botSay()[3], 1200)
+        setTimeout(question.innerHTML = botSay()[3], 1500)
         answer.value = ""
     }else if(init === 4){
-        setTimeout(question.innerHTML = botSay()[4], 1200)
+        setTimeout(question.innerHTML = botSay()[4], 1500)
         answer.value = ""
     }else{
         botEnd()
