9fe8ab87b39021fad1287adecb0985de9a117300
Rahesakomara <109297884+Komara11@users.noreply.github.com>
2025-05-04 02:02:08 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const question = document.getElementById("question")
-const answer = document.getElementById("answer")
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
 
-let init = 0
+let init = 0;
 
 const botSay = () => {
     return [
@@ -10,30 +10,38 @@ const botSay = () => {
         "Ngapain mikirin yang buat gua si, kangen ya sama dia? nanti dibilangin dehh😁",
         "Jadi mau cerita apa hari ini?🤔",
         "Dah ah maless mikir, bye😐"
-    ]
-}
+    ];
+};
 
-question.innerHTML = botSay()[0]
+question.innerHTML = botSay()[0];
 
-function botStart(){
-    init++
-    if (init === 1){
-        setTimeout(question.innerHTML = botSay()[1], 1500)
-        answer.value = ""
-    }else if(init === 2){
-        setTimeout(question.innerHTML = botSay()[2], 1500)
-        answer.value = ""
-    }else if(init === 3){
-        setTimeout(question.innerHTML = botSay()[3], 1500)
-        answer.value = ""
-    }else if(init === 4){
-        setTimeout(question.innerHTML = botSay()[4], 1500)
-        answer.value = ""
-    }else{
-        botEnd()
+function botStart() {
+    init++;
+    if (init === 1) {
+        setTimeout(() => {
+            question.innerHTML = botSay()[1];
+            answer.value = "";
+        }, 1500);
+    } else if (init === 2) {
+        setTimeout(() => {
+            question.innerHTML = botSay()[2];
+            answer.value = "";
+        }, 1500);
+    } else if (init === 3) {
+        setTimeout(() => {
+            question.innerHTML = botSay()[3];
+            answer.value = "";
+        }, 1500);
+    } else if (init === 4) {
+        setTimeout(() => {
+            question.innerHTML = botSay()[4];
+            answer.value = "";
+        }, 1500);
+    } else {
+        botEnd();
     }
 }
 
 const botEnd = () => {
     location.reload();
-}
+};
